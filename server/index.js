3ea6e2580e0278b21943b80021810b11a2afadff
monalishasamal27 <monalisasamal938@gmail.com>
2026-08-11 00:14:25 +0530

    cors errors

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:["http://localhost:3000", "https://study-notion-master-1-cx19.onrender.com"],
 		credentials:true,
 	})
 	);
